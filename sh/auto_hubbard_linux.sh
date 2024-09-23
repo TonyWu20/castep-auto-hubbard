e534f954c7f27e6c2d58b5737579870b4231f4e8
TonyWu20 <tony.w21@gmail.com>
2024-09-23 23:52:44 +0800

    Adjust user experience: mode is 3rd argument, and read mode only require perturb time

--- sh/auto_hubbard_linux.sh
+++ sh/auto_hubbard_linux.sh
@@ -30,38 +30,10 @@ fi
 # Will be reconfirmed for blank or invalid input
 job_type=$2
 
-# Third argument is number of perturbations
-if [[ $3 == '' ]]; then
-	read -r -e -p "Perturb times (default to 5): " PERTURB_TIMES
-	PERTURB_TIMES=${PERTURB_TIMES:-5}
-	echo "Number of perturbation: $PERTURB_TIMES"
-else
-	PERTURB_TIMES=$3
-fi
-
-# Fourth argument is the increment of Hubbard_alpha
-if [[ $4 == '' ]]; then
-	read -r -e -p "Perturb increment (e.g. +0.05/per step): " PERTURB_INCREMENT
-	PERTURB_INCREMENT=${PERTURB_INCREMENT:-0.05}
-	echo "Increment of alpha: $PERTURB_INCREMENT"
-else
-	PERTURB_INCREMENT=$4
-fi
-
-# Fifth argument is the initial U
-if [[ $5 == '' ]]; then
-	read -r -e -p "Init U (non zero, default to 0.000000010000000): " init_u
-elif [[ $5 =~ ^[+-]?[0-9]+\.?[0-9]*$ ]]; then
-	init_u=$5
-else
-	echo "Input init U is not a valid float number; run by default 0.000000010000000"
-fi
-
 # List of running modes for selection
 RUN_MODES=(serial parallel read)
-
-# Sixth  argument is running mode
-if [[ $6 == '' ]]; then
+# Third  argument is running mode
+if [[ $3 == '' ]]; then
 	PS3="Please choose running mode (enter number): "
 	select choice in "${RUN_MODES[@]}"; do
 		case $choice in
@@ -72,26 +44,65 @@ if [[ $6 == '' ]]; then
 		*) echo "Invalid option $choice" ;;
 		esac
 	done
-elif [[ $6 == 'serial' || $6 == 'parallel' || $6 == 'read' ]]; then
-	run_mode=$6
+elif [[ $3 == 'serial' || $3 == 'parallel' || $3 == 'read' ]]; then
+	run_mode=$3
 else
 	echo "Invalid input of running mode (serial/parallel/read), please restart the program"
 	exit
 fi
 
-# PERTURB_TIMES=5
-# PERTURB_INCREMENT=0.05
-#
-
-setup "$init_u" "$init_elec_energy_tol" "$U_increment" "$U_final" "$job_type"
-setup_perturbation "$PERTURB_TIMES" "$PERTURB_INCREMENT"
-setup_castep_command "$castep_command_u" "$castep_command_alpha"
-
-N=32
-
 case $run_mode in
-serial) serial ;;
-parallel) parallel $N ;;
-read) after_read ;;
+serial | parallel)
+	# Fourth argument is the initial alpha value shift in perturbation
+	if [[ $4 == '' ]]; then
+		read -r -e -p "Perturb initial Δalpha (default to 0.05): " init_alpha
+		PERTURB_INIT_ALPHA=${init_alpha:-0.05}
+	elif [[ $4 =~ ^[+-]?[0-9]+\.?[0-9]*$ ]]; then
+		PERTURB_INIT_ALPHA=$4
+	else
+		echo "Input init Δalpha is not a valid float number; run by default 0.05"
+	fi
+
+	# Fifth argument is the increment of Hubbard_alpha
+	if [[ $5 == '' ]]; then
+		read -r -e -p "Perturb increment (e.g. +0.05/per step): " increment
+		PERTURB_INCREMENT=${increment:-0.05}
+		echo "Increment of alpha: $PERTURB_INCREMENT"
+	else
+		PERTURB_INCREMENT=$5
+	fi
+
+	# Fifth argument is the initial U
+	if [[ $6 == '' ]]; then
+		read -r -e -p "Perturb final Δalpha (default to 0.25): " final_alpha
+		PERTURB_FINAL_ALPHA=${final_alpha:-0.25}
+	elif [[ $6 =~ ^[+-]?[0-9]+\.?[0-9]*$ ]]; then
+		PERTURB_FINAL_ALPHA=$6
+	else
+		echo "Input final Δalpha is not a valid float number; run by default 0.25"
+	fi
+
+	PERTURB_TIMES=$(seq "$PERTURB_INIT_ALPHA" "$PERTURB_INCREMENT" "$PERTURB_FINAL_ALPHA" | wc -l)
+	echo "Init Δalpha=$PERTURB_INIT_ALPHA; increment=$PERTURB_INCREMENT; final Δalpha=$PERTURB_FINAL_ALPHA"
+	echo -e "Perturbation times: $PERTURB_TIMES\n"
+	setup "$init_u" "$init_elec_energy_tol" "$U_increment" "$U_final" "$job_type"
+	setup_perturbation "$PERTURB_INIT_ALPHA" "$PERTURB_INCREMENT" "$PERTURB_FINAL_ALPHA"
+	setup_castep_command "$castep_command_u" "$castep_command_alpha"
+
+	N=32
+	case $run_mode in
+	serial) serial ;;
+	parallel) parallel $N ;;
+	*) exit ;;
+	esac
+	;;
+read)
+	if [[ $4 == '' ]]; then
+		read -r -e -p "How many times of perturbation did you set? " PERTURB_TIMES
+	else
+		PERTURB_TIMES=$4
+	fi
+	after_read
+	;;
 *) exit ;;
 esac
