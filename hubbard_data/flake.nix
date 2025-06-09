9ff986fbcefa25b9d025abe1542a5c55bade19bb
TonyWu20 <tony.w21@gmail.com>
2025-06-09 20:45:41 +0800

    A flake script to compile `hubbard_data` for non-NixOS linux distro

--- hubbard_data/flake.nix
+++ hubbard_data/flake.nix
@@ -2,27 +2,43 @@
   description = "A flake to build `hubbard_data` on nixOS";
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    naersk = {
+      url = "github:nix-community/naersk";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
-  outputs = inputs@{ self, nixpkgs, ... }:
-    let
-      system = "x86_64-linux";
-      pkgs = import nixpkgs { inherit system; };
-    in
-    {
-      devShells.${system} = {
-        default = pkgs.mkShell {
-          nativeBuildInputs = with pkgs; [
-            pkg-config
-            fish
-            stdenv
+
+  outputs = { self, fenix, flake-utils, naersk, nixpkgs, ... }:
+    flake-utils.lib.eachDefaultSystem (system: {
+      packages.default =
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+          target = "x86_64-unknown-linux-gnu";
+          toolchain = with fenix.packages.${system}; combine [
+            stable.cargo
+            stable.rustc
+            targets.${target}.stable.rust-std
           ];
-          buildInputs = with pkgs; [
-            fontconfig
-          ];
-          shellHook = ''
-            exec fish
+        in
+        (naersk.lib.${system}.override {
+          cargo = toolchain;
+          rustc = toolchain;
+        }).buildPackage {
+          src = ./.;
+          CARGO_BUILD_TARGET = target;
+          CARGO_TARGET_X86_64_UNKNOWN_LINUX_GNU_LINKER =
+            let
+              inherit (pkgs.pkgsCross.gnu64.stdenv) cc;
+            in
+            "${cc}/bin/${cc.targetPrefix}cc";
+          postInstall = ''
+            patchelf --set-interpreter /usr/lib64/ld-linux-x86-64.so.2 $out/bin/hubbard_data
           '';
         };
-      };
-    };
+    });
 }
