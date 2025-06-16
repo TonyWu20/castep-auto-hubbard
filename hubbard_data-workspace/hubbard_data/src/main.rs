8fdf9f192b2dbe0bda740c553312f3e2140f0f0c
TonyWu20 <tony.w21@gmail.com>
2025-06-16 11:27:55 +0800

    Update to matched `hubbard_data_analyzse`'s changes

--- hubbard_data-workspace/hubbard_data/src/main.rs
+++ hubbard_data-workspace/hubbard_data/src/main.rs
@@ -1,37 +1,30 @@
-use std::{env, fs::File};
-
-use hubbard_data_analyze::{
-    CsvWriter, JobType, SerWriter, get_response, merge_u_alpha_channel_view, view_by_channel_id,
-    view_mean,
+use std::{
+    env,
+    fs::{File, create_dir_all},
 };
+
+use hubbard_data_analyze::{Alpha, CsvWriter, JobType, MergedLazyChannel, SerWriter, U};
 use hubbard_data_args::{HubbardDataCli, Parser};
 
 fn main() -> Result<(), anyhow::Error> {
     let cli = HubbardDataCli::parse();
-    let u_df = get_response(
-        cli.result_folder().unwrap_or(&env::current_dir()?),
-        JobType::U,
-        cli.u_perturb_val(),
-    )?;
-    let alpha_df = get_response(
-        cli.result_folder().unwrap_or(&env::current_dir()?),
-        JobType::Alpha,
-        cli.alpha_perturb_val(),
-    )?;
-    u_df.column("Channel ID")?
-        .unique_stable()?
-        .u32()?
-        .iter()
-        .map(|i| i.unwrap())
-        .try_for_each(|i| {
-            let mut channel_mean_df = merge_u_alpha_channel_view(
-                view_by_channel_id(&u_df, i, JobType::U),
-                view_by_channel_id(&alpha_df, i, JobType::Alpha),
-            )
-            .and_then(view_mean)?;
-            let channel_mean_csv = File::create(format!("channel_{}_mean.csv", i))?;
-            CsvWriter::new(channel_mean_csv).finish(&mut channel_mean_df)?;
-            Ok::<(), anyhow::Error>(())
+    let cwd = env::current_dir()?;
+    let src_dir = cli.result_folder().unwrap_or(&cwd);
+    let df_u = U::csv_path(src_dir).process_data(cli.u_perturb_val())?;
+    let df_alpha = Alpha::csv_path(src_dir).process_data(cli.alpha_perturb_val())?;
+    let merged = MergedLazyChannel::merge_u_alpha_channel_view(&df_u, &df_alpha)?;
+    let dest_dir = src_dir.join("plot");
+    // Create the `dest_dir`, and don't care about the result.
+    // If result is Ok(()) then the directory is successfully created, or it has already existed.
+    create_dir_all(&dest_dir).ok();
+    merged
+        .into_iter()
+        .map(|m| m.view_mean())
+        .enumerate()
+        .try_for_each(|(i, m)| {
+            let file = File::create(dest_dir.join(format!("channel_{}_mean.csv", i + 1)))?;
+            CsvWriter::new(file).finish(&mut m?)
         })?;
+
     todo!()
 }
